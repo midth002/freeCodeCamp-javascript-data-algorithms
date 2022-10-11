b98a4d051cfcdc4c84aeacbc151160dd4563d5e4
Andrew Midthun <midth002@gmail.com>
2022-10-11 14:44:57 -0500

    object mutation and rest parameters.

---
+++ es6-javascript/default-parameters-for-functions.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+
+// Modify the function increment by adding default parameters so that it will add 1 to number if value is not specified.
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
